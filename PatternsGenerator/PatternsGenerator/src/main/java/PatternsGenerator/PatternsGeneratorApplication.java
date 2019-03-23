a1da2205a03a15762a4a4598b3b510e050dd5c9e
Michał Wąsik <mwasik95@gmail.com>
2019-03-23 12:35:40 +0100

    Initial Commit

---
+++ PatternsGenerator/PatternsGenerator/src/main/java/PatternsGenerator/PatternsGeneratorApplication.java
@@ -0,0 +1,13 @@
+package PatternsGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternsGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatternsGeneratorApplication.class, args);
+	}
+
+}
---
+++ PatternsGenerator/PatternsGenerator/src/test/java/PatternsGenerator/PatternsGeneratorApplicationTests.java
@@ -0,0 +1,16 @@
+package PatternsGenerator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PatternsGeneratorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
