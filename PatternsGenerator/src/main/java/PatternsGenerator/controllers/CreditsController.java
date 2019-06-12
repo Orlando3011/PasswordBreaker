df1a9334eff350f1e92712a50e49f1131ab69135
Michał Wąsik <mwasik95@gmail.com>
2019-06-12 15:52:21 +0200

    Added program information page (blank for now) Remover unnecessary imports from controllers

--- PatternsGenerator/src/main/java/PatternsGenerator/controllers/CreditsController.java
+++ PatternsGenerator/src/main/java/PatternsGenerator/controllers/CreditsController.java
@@ -1,16 +1,14 @@
 package PatternsGenerator.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.io.IOException;
 
 @Controller
 public class CreditsController {
 
     @GetMapping("/credits")
-    public String ShowHomePage(Model model) throws IOException {
+    public String ShowHomePage() {
         return "credits";
     }
 }
